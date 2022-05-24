c3d37e37b4b6e69299d9ab8d53f29db74072f326
james23216 <jhmun23216@gmail.com>
2022-05-24 04:08:46 -0700

    clean next.js project, tailwind css config done, redux done, sort the structure, package.json done - [10.3]

---
+++ src/pages/_app.tsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react";
+import Script from 'next/script';
+import "../../styles/globals.css";
+import type { AppProps } from "next/app";
+import Header from "../components/layouts/header/header";
+import Footer from "../components/layouts/Footer/footer";
+import { store } from '../components/redux/store';
+import { Provider } from 'react-redux';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    setTimeout(function () {
+      setLoading(false);
+    }, 250);
+
+    return () => {
+      setLoading(true);
+    };
+  }, [pageProps]);
+
+  return (
+    <>
+      <Provider store={store}>
+        <Header />
+
+        <section
+          className={`${loading === true ? "dark:animate-none animate-Loading " : ""}main-section`}
+        >
+          <Component {...pageProps} />
+        </section>
+
+        <Footer />
+      </Provider>
+    </>
+  );
+}
+export default MyApp;
